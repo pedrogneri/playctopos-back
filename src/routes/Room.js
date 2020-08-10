6d896530ba316394b80e9081304dd28d9a8f2e60
Pedro Neri <pedrogneri@gmail.com>
2020-08-10 02:27:42 -0300

    Revert previous mod

--- src/routes/Room.js
+++ src/routes/Room.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const RoomController = require('controllers/RoomController');
 
-router.get('room', RoomController.show);
-router.post('room', RoomController.update);
+router.get('/room', RoomController.show);
+router.post('/room', RoomController.update);
 
 module.exports = router;

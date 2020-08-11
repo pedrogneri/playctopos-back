c5147aba16aa88eb62972f9a0382b6cb45689031
Pedro Neri <pedrogneri@gmail.com>
2020-08-11 18:59:58 -0300

    Create getVideoUrlByRoom route

--- src/routes/Room.js
+++ src/routes/Room.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const RoomController = require('controllers/RoomController');
 
 router.get('/room', RoomController.show);
-router.post('/room', RoomController.update);
+router.put('/room', RoomController.update);
+
+router.get('/getVideoUrlByRoom', RoomController.getVideoUrlByRoom);
 
 module.exports = router;

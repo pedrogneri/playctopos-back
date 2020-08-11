c1d5b02a05a28035aaa9a08f7ce77b8c85eb2eb0
Pedro Neri <pedrogneri@gmail.com>
2020-08-11 18:59:35 -0300

    Create getVideoUrlByRoom function on roomController

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -15,8 +15,10 @@ const show = (req, res) => {
 const update = (req, res) => {
   const { id } = req.query;
   const room = req.body;
+  const now = new Date().getTime();
+  const updatedRoom = { ...room, lastPlayDate: now };
 
-  Room.findOneAndUpdate({ _id: id }, room)
+  Room.findOneAndUpdate({ _id: id }, updatedRoom)
     .then((result) => {
       res.status(200).send(result);
     })
@@ -25,7 +27,35 @@ const update = (req, res) => {
     });
 };
 
+const getVideoUrlByRoom = (req, res) => {
+  const { id } = req.query;
+
+  Room.findById(id)
+    .then((room) => {
+      const videoUrl = generateVideoURL(room);
+      res.status(200).send(videoUrl);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+};
+
+const generateVideoURL = ({ actualVideoId, lastPlayDate }) => {
+  const videoTime = Math.round((new Date().getTime() - lastPlayDate) / 1000);
+
+  if (actualVideoId !== '' && lastPlayDate !== '') {
+    return {
+      url: `https://www.youtube.com/embed/${actualVideoId}?controls=0&rel=0&cc_load_policy=0&showinfo=0&start=${videoTime}`,
+    };
+  } else {
+    return {
+      url: '',
+    };
+  }
+};
+
 module.exports = {
   show,
   update,
+  getVideoUrlByRoom,
 };

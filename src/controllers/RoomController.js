bd0e01b1640d5cbd43b82c119f2410ba3a2871e1
Pedro Neri <pedrogneri@gmail.com>
2020-08-23 20:35:37 -0300

    Add playlist

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -43,24 +43,57 @@ const getVideoUrlByRoom = (req, res) => {
   const { id } = req.query;
 
   Room.findById(id)
-    .then((room) => {
-      const url = generateVideoURL(room);
-      res.status(200).send({ room, url });
+    .then(async (room) => {
+      const url = await generateVideoURL(room);
+      const newRoom = await Room.findById(id);
+      res.status(200).send({ room: newRoom, url });
     })
     .catch((err) => {
+      console.log(err);
       res.status(500).send(err);
     });
 };
 
-const generateVideoURL = ({ actualVideo: { id }, lastPlayDate }) => {
-  const videoTime = Math.round((new Date().getTime() - lastPlayDate) / 1000);
+const changeToNextSong = async (room) => {
+  const now = new Date().getTime();
+  const nextVideo = room.playlist[0];
+  const newPlaylist = room.playlist;
+  newPlaylist.shift();
 
-  if (id !== '' && lastPlayDate !== '') {
-    return `https://www.youtube.com/embed/${id}?controls=0&rel=0&cc_load_policy=0&showinfo=0&start=${videoTime}`;
+  const updatedRoom = {
+    playlist: newPlaylist,
+    actualVideo: {
+      id: nextVideo.id,
+      title: nextVideo.title,
+      channel: nextVideo.channel,
+      thumbnail: nextVideo.thumbnail,
+    },
+    lastPlayDate: now,
+  };
+
+  await Room.findOneAndUpdate({ _id: room._id }, updatedRoom);
+
+  return updatedRoom;
+};
+
+const generateVideoURL = async (room) => {
+  const videoTime = calculateActualVideoTime(room.lastPlayDate);
+  const nextVideo = room.playlist.length > 0 ? room.playlist[0] : undefined;
+
+  if (room.actualVideo.id !== '' && room.lastPlayDate !== '') {
+    return `https://www.youtube.com/embed/${room.actualVideo.id}?controls=0&rel=0&cc_load_policy=0&showinfo=0&start=${videoTime}`;
+  } else if (!!nextVideo) {
+    const newRoom = await changeToNextSong(room);
+
+    return `https://www.youtube.com/embed/${
+      newRoom.actualVideo.id
+    }?controls=0&rel=0&cc_load_policy=0&showinfo=0&start=${calculateActualVideoTime(newRoom.lastPlayDate)}`;
   } else {
     return '';
   }
 };
+
+const calculateActualVideoTime = (lastPlayDate) => Math.round((new Date().getTime() - lastPlayDate) / 1000);
 
 module.exports = {
   show,

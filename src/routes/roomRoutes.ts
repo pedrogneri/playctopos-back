1da83e5049d40a282d78600bcce4e445e0c43b20
Pedro Neri <pedrogneri@gmail.com>
2021-01-31 03:33:26 -0300

    Remove routes from room to test heroku deploy

--- src/routes/roomRoutes.ts
+++ src/routes/roomRoutes.ts
@@ -1,17 +1,20 @@
 import { Router } from 'express';
-import {
-  getRoomByName,
-  getVideoUrlByRoom,
-  updateActualVideo,
-  updatePlaylist,
-} from '../controllers/RoomController';
+// import {
+//   getRoomByName,
+//   getVideoUrlByRoom,
+//   updateActualVideo,
+//   updatePlaylist,
+// } from '../controllers/RoomController';
 
 const router = Router();
 
-router.put('/actualVideo', updateActualVideo);
-router.put('/playlist', updatePlaylist);
+// router.put('/actualVideo', updateActualVideo);
+// router.put('/playlist', updatePlaylist);
 
-router.get('/getVideoUrlByRoom', getVideoUrlByRoom);
-router.get('/getRoomByName', getRoomByName);
+// router.get('/getVideoUrlByRoom', getVideoUrlByRoom);
+// router.get('/getRoomByName', getRoomByName);
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+router.get('/getRoomByName', () => {});
 
 export default router;
